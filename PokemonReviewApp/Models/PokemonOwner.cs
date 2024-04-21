e58a3d6daf7e4bd56f0a07ff2132b5e01aa63dff
Mohammed Younes <mohammed.alyounes@gmail.com>
2024-04-21 14:59:56 +0300

    starting

---
+++ PokemonReviewApp/Models/PokemonOwner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PokemonReviewApp.Models
+{
+	public class PokemonOwner
+	{
+		public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+		public Pokemon Pokemom { get; set; }
+		public Owner Owner { get; set; }
+
+	}
+}
+
