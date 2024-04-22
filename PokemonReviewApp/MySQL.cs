282687f866f768410a565f8575fc8d9d91e6f9e1
Mohammed Younes <mohammed.alyounes@gmail.com>
2024-04-22 13:13:20 +0300

    trying on a windows

---
+++ PokemonReviewApp/MySQL.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using Microsoft.Data.SqlClient;
+//using MySql.Data.MySqlClient;
+
+
+//namespace PokemonReviewApp
+//{
+//    public class MySQL
+//	{
+//        static MySqlConnection con;
+
+//        public static void Connect(string user, string password)
+//        {
+//            con = new MySqlConnection();
+
+//            try
+//            {
+//                con.ConnectionString = "server = localhost; User Id = " + user + "; " +
+//                    "Persist Security Info = True; database = hello; Password = " + password;
+//                con.Open();
+//                Console.WriteLine("Succesfully connected!");
+
+//            }
+
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("Not Successful! due to " + e.ToString());
+//            }
+//        }
+
+//    }
+//}
+
