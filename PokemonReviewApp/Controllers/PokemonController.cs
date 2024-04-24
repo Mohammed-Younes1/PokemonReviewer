4b9168167fbfa023ac4adaa1d56f286420978d84
Mohammed-Younes1 <mohammed.alyounes@gmail.com>
2024-04-24 14:54:47 +0300

    adding the first controller

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interface;
 using PokemonReviewApp.Models;
+using System.Collections.Generic;
 
 namespace PokemonReviewApp.Controllers
 {
@@ -9,10 +12,12 @@ namespace PokemonReviewApp.Controllers
     public class PokemonController : Controller
     {
         private IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
 
-        public PokemonController(IPokemonRepository pokemonRepository)
+        public PokemonController(IPokemonRepository pokemonRepository,IMapper mapper)
         {
             _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
         }
         [HttpGet]
         [ProducesResponseType(200,Type= typeof(IEnumerable<Pokemon>))]
@@ -28,7 +33,7 @@ namespace PokemonReviewApp.Controllers
         //}
         public IActionResult GetPokemon()
         {
-            var Pokemons = _pokemonRepository.GetPokemons();
+            var Pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
             if (Pokemons == null || !Pokemons.Any())
             {
                 return NotFound(); // Return 404 if no pokemons are found
@@ -36,5 +41,43 @@ namespace PokemonReviewApp.Controllers
 
             return Ok(Pokemons);
         }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200,Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId) 
+        { 
+            if(!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(rating);
+
+        }
+
     }
 }
--- PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d122676081ce2ac1420cc298390b9a6fba98281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2214d55774397605987dbe9c1432a938352a495a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
